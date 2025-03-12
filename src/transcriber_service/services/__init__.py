1b8e70598736b2677e886ad296662e000663502a
Tanya Kozlova <tanya120606@mail.ru>
2025-03-13 00:54:33 +0300

    change init for services

--- src/transcriber_service/services/__init__.py
+++ src/transcriber_service/services/__init__.py
@@ -1,10 +1,7 @@
-from .auth import AuthService
-from .storage_service import StorageService
 from .processing import WhisperProcessor
 from .storage_service import StorageService
 from .auth_service import AuthService
 from .processing import WhisperProcessor
 from .audio_service import AudioService
-from .record_tag_service import RecordTagService
 
-__all__ = ['WhisperProcessor', 'AuthService', 'StorageService', 'AudioService', 'RecordTagService']
+__all__ = ['WhisperProcessor']
