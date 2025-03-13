459d9fb9d003a6b2a0c4ef7c0190d6279892b5e9
Tanya Kozlova <tanya120606@mail.ru>
2025-03-13 22:42:46 +0300

    Add checking security for password (len >= 8, one uppercase, number and special symbol)

--- src/transcriber_service/services/__init__.py
+++ src/transcriber_service/services/__init__.py
@@ -1,7 +1,6 @@
 from .processing import WhisperProcessor
 from .storage_service import StorageService
 from .auth_service import AuthService
-from .processing import WhisperProcessor
 from .audio_service import AudioService
 
-__all__ = ['WhisperProcessor']
+__all__ = ['WhisperProcessor', 'StorageService', 'AuthService', 'AudioService']
--- src/transcriber_service/services/auth_service.py
+++ src/transcriber_service/services/auth_service.py
@@ -1,3 +1,5 @@
+from password_strength import PasswordPolicy
+
 from ..domain import AuthUser, AuthException, Admin, User
 from ..repositories import IUserRepository
 from .storage_service import StorageService
@@ -8,10 +10,14 @@ class AuthService:
         self.__users = {}
         self.__user_repo = repo
         self.__storage_service = storage_service
+        self.__policy = PasswordPolicy.from_names(length=8, uppercase=1, numbers=1, special=1)
 
     def register_user(self, email: str, password: str) -> AuthUser:
         if self.__user_repo.get_by_email(email):
-            raise AuthException("User already exists")
+            raise AuthException('User already exists')
+
+        if self.__policy.test(password):
+            raise AuthException('Password is weak')
 
         user = AuthUser(email, password)
         self.__user_repo.add(user)
@@ -22,7 +28,8 @@ class AuthService:
 
     def create_admin(self, email: str, password: str) -> Admin:
         if self.__user_repo.get_by_email(email):
-            raise AuthException("User already exists")
+            raise AuthException('User already exists')
+
         user = Admin(email, password)
         self.__user_repo.add(user)
         return user
@@ -30,13 +37,16 @@ class AuthService:
     def login(self, email: str, password: str) -> User | None:
         user = self.__user_repo.get_by_email(email)
         if not user or not user.verify_password(password):
-            raise AuthException("Invalid credentials")
+            raise AuthException('Invalid credentials')
         return user
 
     def change_password(self, email: str, current_password: str, new_password: str) -> None:
         user = self.__user_repo.get_by_email(email)
         if not user:
-            raise AuthException("User not found")
+            raise AuthException('User not found')
+
+        if self.__policy.test(new_password):
+            raise AuthException('Password is weak')
 
         try:
             user.change_password(current_password, new_password)
