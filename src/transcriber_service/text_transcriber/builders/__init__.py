59f0f1be3e2115af3d1c0c639d9273cfeaaefe12
Tanya Kozlova <tanya120606@mail.ru>
2025-04-10 01:13:13 +0300

    Add docstring for builders.

--- src/transcriber_service/text_transcriber/builders/__init__.py
+++ src/transcriber_service/text_transcriber/builders/__init__.py
@@ -1,3 +1,15 @@
+"""
+Package: text_transcriber.builders
+-----------------------------------
+
+This package provides utility classes for directing and managing audio-related
+processing tasks, such as transcription and speaker diarization.
+
+Modules:
+    - transcribe_processor_director: Provides `TranscribeProcessorDirector` for transcription tasks.
+    - voice_separator_director: Provides `VoiceSeparatorDirector` for speaker diarization.
+"""
+
 from .transcribe_processor_director import *
 from .voice_separator_director import *
 
