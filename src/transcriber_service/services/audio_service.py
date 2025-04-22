e0028b9fb76cebf87b60156ee215f5656bc061e3
Tanya Kozlova <tanya120606@mail.ru>
2025-04-22 20:26:41 +0300

    Refactor method signatures and update transcribe logic.

--- src/transcriber_service/services/audio_service.py
+++ src/transcriber_service/services/audio_service.py
@@ -13,41 +13,45 @@ class AudioService(object):
     transcription processing.
     """
 
-    def __init__(self,
-                 repo: IAudioRepository,
-                 exporter: TextExporter = TextExporter()):
+    def __init__(
+            self,
+            repo: IAudioRepository,
+            exporter: TextExporter = TextExporter()
+    ):
         self.__audio_repo = repo
         self.__exporter = exporter
         self.__stopwords_remover = NatashaStopwordsRemover()
         self.__transcriber = Transcriber(Config.PYANNOTE_TOKEN)
 
-    def create_audio(self,
-                     file_name: str,
-                     content: bytes,
-                     file_path: str,
-                     storage_id: str,
-                     language: str = None,
-                     main_theme: str = None,
-                     ) -> AudioRecord:
+    def create_audio(
+            self,
+            file_name: str,
+            content: bytes,
+            file_path: str,
+            storage_id: str,
+            language: str = None,
+            main_theme: str = None,
+    ) -> AudioRecord:
         """
-        Create AudioRecord instance with basic metadata and do transcription into text with given transcribe service.
+        Create AudioRecord instance with basic metadata and do transcription
+        into text with given transcribe service.
 
         :param file_name: Name of audio file.
         :param content: Content of audio file (mp3).
-        :param file_path: Full path to audio file in some storage directory (not user storage).
+        :param file_path: Full path to audio file in some storage directory
+        (not user storage).
         :param storage_id: Storage id of audio file.
         :param language: Language of audio file (defaults None).
         :param main_theme: Main theme of audio file (defaults None).
         :return: Created Audio Record.
         """
 
-        text, language = self.__transcriber.transcribe(content, language)
+        text, language = self.__transcriber.transcribe(content, language, main_theme)
         audio = AudioRecord(file_name, file_path, storage_id, text, language)
         self.__audio_repo.add(audio)
         return audio
 
-    def get_records(self,
-                    storage_id: str) -> tuple | None:
+    def get_records(self, storage_id: str) -> tuple | None:
         """
         Retrieves audio record by its storage container ID.
 
@@ -57,9 +61,7 @@ class AudioService(object):
 
         return self.__audio_repo.get_by_storage(storage_id)
 
-    def add_tag_to_record(self,
-                          record_id: str,
-                          tag: str) -> None:
+    def add_tag_to_record(self, record_id: str, tag: str) -> None:
         """
         Add tag to record by its ID.
 
@@ -75,9 +77,7 @@ class AudioService(object):
         else:
             raise ValueError('Record not found')
 
-    def remove_tag_from_record(self,
-                               record_id: str,
-                               tag: str) -> None:
+    def remove_tag_from_record(self, record_id: str, tag: str) -> None:
         """
         Remove tag from record by its ID and tag name.
 
@@ -96,9 +96,7 @@ class AudioService(object):
         else:
             raise ValueError('Record not found')
 
-    def change_record_name(self,
-                           record_id: str,
-                           name: str) -> None:
+    def change_record_name(self, record_id: str, name: str) -> None:
         """Change name of record by its ID."""
 
         record = self.__audio_repo.get_by_id(record_id)
@@ -106,10 +104,12 @@ class AudioService(object):
         record.record_name = name
         self.__audio_repo.update(record)
 
-    def export_record_text(self,
-                           record_id: str,
-                           output_dir: str,
-                           file_format: str) -> None:
+    def export_record_text(
+            self,
+            record_id: str,
+            output_dir: str,
+            file_format: str
+    ) -> None:
         """
         Export text of audio record by its ID.
 
@@ -125,10 +125,11 @@ class AudioService(object):
 
         self.__exporter.export_text(record.text, output_dir, record.record_name, file_format)
 
-    def remove_stopwords(self,
-                         record_id: str,
-                         remove_swear_words: bool = True,
-                         go_few_times: bool = False) -> None:
+    def remove_stopwords(
+            self,
+            record_id: str,
+            remove_swear_words: bool = True,
+            go_few_times: bool = False) -> None:
         """
         Remove stopwords from audio record by its ID.
 
@@ -148,11 +149,11 @@ class AudioService(object):
         if record.language.lower() != 'ru' and record.language.lower() != 'russian':
             raise ValueError(f'Unsupported language: {record.language}')
 
-        record.text = self.__stopwords_remover.remove_stopwords(record.text, remove_swear_words)
+        record.text = self.__stopwords_remover.remove_stopwords(
+            record.text, remove_swear_words, go_few_times
+        )
 
-    def remove_words(self,
-                     record_id: str,
-                     words: list | tuple) -> None:
+    def remove_words(self, record_id: str, words: list | tuple) -> None:
         """
         Remove words from audio record by its ID with given words.
 
