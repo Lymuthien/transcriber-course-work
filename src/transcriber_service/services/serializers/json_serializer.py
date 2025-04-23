37b2d0bd28ce46250d7d98ef79e137c0cc2a0acf
Tanya Kozlova <tanya120606@mail.ru>
2025-04-23 23:08:43 +0300

    Add serializers

---
+++ src/transcriber_service/services/serializers/json_serializer.py
@@ -0,0 +1,16 @@
+import json
+
+from ...interfaces.iserializer import ISerializer
+
+
+class JsonSerializer(ISerializer):
+    def serialize(self, data) -> str:
+        return json.dumps(data)
+
+    def deserialize(self, data: str):
+        data_dict = json.loads(data)
+        return data_dict
+
+    @property
+    def extension(self) -> str:
+        return 'json'
---
+++ src/transcriber_service/services/serializers/pickle_serializer.py
@@ -0,0 +1,15 @@
+import pickle
+
+from ...interfaces.iserializer import ISerializer
+
+
+class PickleSerializer(ISerializer):
+    def serialize(self, data) -> bytes:
+        return pickle.dumps(data)
+
+    def deserialize(self, data: bytes):
+        return pickle.loads(data)
+
+    @property
+    def extension(self) -> str:
+        return 'pickle'
